0eb0495ffbc7985f4f16b376c77db04566e1ff35
jtrbg <joshbrullcollege@gmail.com>
2024-05-06 04:56:50 -0400

    Missed removal of mod function

--- keymanager.py
+++ keymanager.py
@@ -2,9 +2,6 @@ from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.backends import default_backend
 
-def mod(left, right):
-    return left % right
-
 def gcd(a, b):
     sa = a
     sb = b
